6dd30dab188a21c3c148fff4d53c0df4bbdcc23e
Cyoungju <dudwn3528@gmail.com>
2024-08-23 15:38:12 +0900

    resilience4j, retry, timelimiter 설정

--- order-service/src/main/java/com/example/orderservice/client/MemberServiceClient.java
+++ order-service/src/main/java/com/example/orderservice/client/MemberServiceClient.java
@@ -1,8 +1,6 @@
 package com.example.orderservice.client;
 
-
 import com.example.orderservice.dto.AddressResponseDto;
-import com.example.orderservice.dto.MemberResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,4 +15,5 @@ public interface MemberServiceClient {
 
     @GetMapping("/api/client/address/{addressId}")
     Optional<AddressResponseDto> getAddressById(@PathVariable("addressId") Long addressId);
+
 }
